56e1b1c67e0b4f94fe8a07dccb3b29285fa4f619
Folke Henning Gleumes <contact@gleumes.org>
2018-04-10 17:50:36 +0200

    add status indicator

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,12 +1,14 @@
 if [ $TERM != 'linux' ]; then
-	POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator context ssh dir vcs)
-	POWERLEVEL9K_DISABLE_RPROMPT=true
+    POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(status root_indicator context ssh dir dir_writable vcs)
+    POWERLEVEL9K_DISABLE_RPROMPT=true
 
-	# directory folding
-	POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-	POWERLEVEL9K_SHORTEN_DELIMITER=""
-	POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
+    # directory folding
+    POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+    POWERLEVEL9K_SHORTEN_DELIMITER=""
+    POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
 
-	# colorizing prompt
-	POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND='white'
+    # colorizing prompt
+    POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND='white'
+
+    POWERLEVEL9K_STATUS_OK=false
 fi
